72b5004aae599fd64204ee71e5624c13d3e52d49
Blawuken <jarbull87@gmail.com>
2024-01-07 00:33:40 +0700

    feat: Add feature network and bandwitch switcher

---
+++ play-services-core/src/main/java/com/neko/other/BandwitchSwitcher.java
@@ -0,0 +1,41 @@
+package com.neko.other;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.net.Uri;
+
+public class BandwitchSwitcher extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        try
+        {
+            super.onCreate(savedInstanceState);
+            Intent BandInfo = new Intent("android.intent.action.MAIN");
+            BandInfo.setClassName("com.android.settings", "com.android.settings.BandMode");
+            startActivity(BandInfo);
+            finish();
+        }
+        catch (Exception e)
+        {
+            Builder ErrorMessage = new Builder(this);
+            ErrorMessage.setMessage("Sorry, this feature is not available for your device.");
+            ErrorMessage.setPositiveButton("I UNDERSTAND", new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int id)
+                {
+                    dialog.dismiss();
+                    Intent Uninstall = new Intent(Intent.ACTION_DELETE);
+                    Uninstall.setData(Uri.parse("package:com.mgoogle.android.gms"));
+                    startActivity(Uninstall);
+                    finish();
+                }
+            });
+            ErrorMessage.show();
+        }
+    }
+}
