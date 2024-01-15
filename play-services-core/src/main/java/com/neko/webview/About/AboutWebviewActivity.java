e77e368f13ae00d05b15d38042d178700a977d78
Blawuken <jarbull87@gmail.com>
2024-01-15 08:07:55 +0700

    feat: Improved layout and Add new Dev option
    • Add changelog
    • Add about me

---
+++ play-services-core/src/main/java/com/neko/webview/About/AboutWebviewActivity.java
@@ -0,0 +1,39 @@
+package com.neko.webview.About;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import com.google.android.gms.R;
+
+public class AboutWebviewActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    @SuppressLint("SetJavaScriptEnabled")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uwu_webview);
+        mWebView = findViewById(R.id.uwu_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new MyWebViewClient());
+
+        // REMOTE RESOURCE
+        mWebView.loadUrl("https://blawuken.github.io/About/");
+
+        // LOCAL RESOURCE
+        // mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
